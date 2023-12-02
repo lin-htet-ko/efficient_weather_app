b900c6afdb8a8f0e3307520f115875304a40831d
linhtetko101 <linhtetko@myansis.com>
2023-12-03 04:56:10 +0630

    Add Navigation within screens

---
+++ app/src/main/java/com/linhtetko/efficientweatherapp/EfficientWeatherApp.kt
@@ -0,0 +1,8 @@
+package com.linhtetko.efficientweatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EfficientWeatherApp: Application() {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "EfficientWeatherApp"
 include(":app")
+include(":network")
